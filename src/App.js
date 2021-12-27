74fb768d8d7b09bef3c85373b6cb744250e2e730
AxlEstevez <axlestevez@hotmail.com>
2021-12-27 16:08:45 -0600

    src

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+  Ver como abrir un modal para poder editar y agregar nuevos usuarios
+  Filtrar la tabla
+  extra: paginacion de tabla
+*/
+import React, { Component } from 'react';
+import Nav from './components/Nav';
+import TableData from './components/TableData';
+class DataTable extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <TableData />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default DataTable;
